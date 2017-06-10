ece2385d25f9de2490b08f211ab0b573123eef4b
Unknown <andyzam2505@gmail.com>
2017-06-09 18:57:39 -0700

    Added cout

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+  cout << "Hi my name is andy " << endl;
+  cout << "This is my first program " << endl;
+  cout << "this is the third line " << endl;
+
+  return 0;
+}
