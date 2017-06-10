951b109773fba73b12de00967859e7a32653d0d4
Unknown <andyzam2505@gmail.com>
2017-06-09 19:25:23 -0700

    hi

--- main.cpp
+++ main.cpp
@@ -2,10 +2,7 @@
 using namespace std;
 
 int main(){
-
-  cout << "Hi my name is andy " << endl;
-  cout << "This is my first program " << endl;
-  cout << "this is the third line " << endl;
+  cout << "wiliam is a savage 21" << endl;
 
   return 0;
 }
